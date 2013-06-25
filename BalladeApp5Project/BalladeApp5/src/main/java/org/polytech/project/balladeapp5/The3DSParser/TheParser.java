590ae19d08dc649ed510b77390cb2b662c6dda3e
sullivan.matas <sullivan.matas@gmail.com>
2013-06-25 12:58:47 +0000

    Suppression des debugs et ajout du parametre a parseIt. utilisation : TheParser().parseIt("sphere.3ds"); où sphere.3ds est le chemin vers le fichier. Si plantage, alors retourne une liste vide de maillages.

--- BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/The3DSParser/TheParser.java
+++ BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/The3DSParser/TheParser.java
@@ -9,8 +9,8 @@ import org.polytech.project.balladeapp5.Vector3;
 
 public class TheParser {
 
-	public ArrayList<Mesh> parseIt() {
-		File file = new File("sphere.3ds");
+	public ArrayList<Mesh> parseIt(String path) {
+		File file = new File(path);
 
 		Model model = null;
 		try {
@@ -19,48 +19,56 @@ public class TheParser {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		ArrayList<Mesh> objects = new ArrayList<Mesh>();
+		if (file == null || model == null) {
+			return objects;
+		}
+
 		for (ModelObject o : model.objects) {
-			System.out.println(o.getName());
 			objects.add(convert(o));
-			break;
 		}
 		return objects;
 	}
 
 	public Mesh convert(ModelObject object) {
 		Mesh m = new Mesh();
-		ArrayList<Vector3> points = new ArrayList<Vector3>();
-		System.out.println(object.vertices.length);
-		for (int i = 0; i < object.vertices.length; i += 3) {
-			// Vector3 v = ;
-			// points.add(v);
-		}
-		System.out.println(object.polygons.length);
 		for (int i = 0; i < object.polygons.length; i += 3) {
+			// premier point
 			float x = object.vertices[object.polygons[i]];
 			float y = object.vertices[object.polygons[i] + 1];
 			float z = object.vertices[object.polygons[i] + 2];
 			Vector3 p1 = new Vector3(x, y, z);
+			// second point
 			x = object.vertices[object.polygons[i + 1]];
 			y = object.vertices[object.polygons[i + 1] + 1];
 			z = object.vertices[object.polygons[i + 1] + 2];
 			Vector3 p2 = new Vector3(x, y, z);
+			// troisieme point
 			x = object.vertices[object.polygons[i + 2]];
 			y = object.vertices[object.polygons[i + 2] + 1];
 			z = object.vertices[object.polygons[i + 2] + 2];
 			Vector3 p3 = new Vector3(x, y, z);
+			// ajout des points (issus des polygones) au maillage
+			// (dans le bon sens pour l'affichage)
 			m.vertices.add(p1);
 			m.vertices.add(p2);
 			m.vertices.add(p3);
+			// recherche de la normale
+			Vector3 n1 = new Vector3(p2.x() - p1.x(), p2.y() - p1.y(), p2.z()
+					- p1.z());
+			Vector3 n2 = new Vector3(p2.x() - p1.x(), p2.y() - p1.y(), p2.z()
+					- p1.z());
+			m.normals.add(n1.cross(n2));
 		}
 
 		for (int i = 0; i < object.textureCoordinates.length; i += 2) {
+			// recuperation des coordonnees de texture
 			float x = object.textureCoordinates[i];
 			float y = object.textureCoordinates[i + 1];
 			m.texCoords.add(new Vector2(x, y));
+			m.colors.add(new Vector3(200, 200, 200));
 		}
-		m.colors.add(new Vector3(200, 200, 200));
 		return m;
 	}
 }
