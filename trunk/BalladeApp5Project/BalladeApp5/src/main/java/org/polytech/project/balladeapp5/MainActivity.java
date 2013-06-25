1dc68aec184b484c73539fc263c70859e9332ef0
sullivan.matas@gmail.com <sullivan.matas@gmail.com@f0f6e293-ef9f-397d-6bf8-9a079c794fd8>
2013-06-25 12:11:46 +0000

    Ajout de THE killing 3DS Parser. utiliser TheParser.parseIt(); pour tester le parsing d'un fichier 3DS.

--- trunk/BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/MainActivity.java
+++ trunk/BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/MainActivity.java
@@ -1,102 +1,89 @@
 package org.polytech.project.balladeapp5;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
-import java.io.InputStream;
+import java.awt.Menu;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.io.File;
 
-import org.polytech.project.balladeapp5.KMLParser.Placemark;
+import org.polytech.project.balladeapp5.KMLParser.Kml;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
-import org.polytech.project.balladeapp5.KMLParser.Kml;
-
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-    }
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
+	@Override
+	protected void onStart() {
+		super.onStart();
 
-        InputStream is = null;
+		InputStream is = null;
 
-        System.out.println("toto");
+		System.out.println("toto");
 
-        File file = new File("/sdcard/KML_Samples.kml");
+		File file = new File("/sdcard/KML_Samples.kml");
 
-        try
-        {
-            is = new FileInputStream(file);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+		try {
+			is = new FileInputStream(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		Serializer serializer = new Persister();
+		Kml kml = null;
 
-        Serializer serializer = new Persister();
-        Kml kml = null;
+		try {
+			kml = serializer.read(Kml.class, is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        try {
-            kml = serializer.read(Kml.class, is);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		if (kml.getDocument() != null) {
+			List<String> allCoordinates = kml.getDocument().getAllCoordinates();
+			List<Mesh> finalMesh = updateMesh(allCoordinates);
+			System.out.println(finalMesh.size());
+			for (Mesh mesh : finalMesh) {
+				System.out.println(mesh.vertices.get(0).x());
+			}
+		}
+	}
 
-        if(kml.getDocument() != null)
-        {
-            List<String> allCoordinates = kml.getDocument().getAllCoordinates();
-            List<Mesh> finalMesh = updateMesh(allCoordinates);
-            System.out.println(finalMesh.size());
-            for(Mesh mesh : finalMesh)
-            {
-                System.out.println(mesh.vertices.get(0).x());
-            }
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	public static List<Mesh> updateMesh(List<String> coordList) {
+		List<Mesh> meshList = new ArrayList<Mesh>();
+		for (String current : coordList) {
+			String[] vertices = current.split("\n");
+			List<String> verticesList = Arrays.asList(vertices);
+			Mesh temp = new Mesh();
 
-    public static List<Mesh> updateMesh(List<String> coordList)
-    {
-        List<Mesh> meshList = new ArrayList<Mesh>();
-        for(String current : coordList)
-        {
-            String [] vertices= current.split("\n");
-            List<String> verticesList = Arrays.asList(vertices);
-            Mesh temp = new Mesh();
+			for (String currentVertex : verticesList) {
+				String[] returnValue = currentVertex.split(",");
+				List<String> list = Arrays.asList(returnValue);
+				int isOk = list.size() % 3;
+				if (isOk == 0) {
+					Vector3 test = new Vector3(Double.parseDouble(list.get(0)),
+							Double.parseDouble(list.get(1)),
+							Double.parseDouble(list.get(2)));
+					temp.vertices.add(test);
+				}
+			}
+			meshList.add(temp);
+		}
 
-            for(String currentVertex : verticesList)
-            {
-                String [] returnValue = currentVertex.split(",");
-                List<String> list = Arrays.asList(returnValue);
-                int isOk = list.size()%3;
-                if(isOk == 0)
-                {
-                    Vector3 test = new Vector3(Double.parseDouble(list.get(0)), Double.parseDouble(list.get(1)), Double.parseDouble(list.get(2)));
-                    temp.vertices.add(test);
-                }
-            }
-            meshList.add(temp);
-        }
-
-        return meshList;
-    }
+		return meshList;
+	}
 }
--- trunk/BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/Mesh.java
+++ trunk/BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/Mesh.java
@@ -5,24 +5,22 @@ import java.util.ArrayList;
 /**
  * Created by remipraud on 18/06/13.
  */
-public class Mesh
-{
-    public ArrayList<Vector3> vertices;
-    public ArrayList<Vector2> texCoords;
-    public ArrayList<Vector3> normals;
-    public ArrayList<Vector3> colors;
+public class Mesh {
+	public ArrayList<Vector3> vertices;
+	public ArrayList<Vector2> texCoords;
+	public ArrayList<Vector3> normals;
+	public ArrayList<Vector3> colors;
 
-    // Indices for vertex/texcoord/normal values in the
-    // vertices/texCoords/normals arrays
-    ArrayList<Index> indices;
+	// Indices for vertex/texcoord/normal values in the
+	// vertices/texCoords/normals arrays
+	public ArrayList<Index> indices;
 
-    public Mesh()
-    {
-        vertices = new ArrayList<Vector3>();
-        texCoords = new ArrayList<Vector2>();
-        normals = new ArrayList<Vector3>();
-        colors = new ArrayList<Vector3>();
+	public Mesh() {
+		vertices = new ArrayList<Vector3>();
+		texCoords = new ArrayList<Vector2>();
+		normals = new ArrayList<Vector3>();
+		colors = new ArrayList<Vector3>();
 
-        indices = new ArrayList<Index>();
-    }
+		indices = new ArrayList<Index>();
+	}
 }
