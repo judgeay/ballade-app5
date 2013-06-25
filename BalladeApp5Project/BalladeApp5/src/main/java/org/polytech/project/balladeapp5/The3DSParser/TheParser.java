f4bf4f41cf227f47b65f1520222695a2a0542568
sullivan.matas <sullivan.matas@gmail.com>
2013-06-25 12:46:35 +0000

    Ajout des coordonnees de textures et d'une couleur gris clair par défaut.

--- BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/The3DSParser/TheParser.java
+++ BalladeApp5Project/BalladeApp5/src/main/java/org/polytech/project/balladeapp5/The3DSParser/TheParser.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 import org.polytech.project.balladeapp5.Mesh;
+import org.polytech.project.balladeapp5.Vector2;
 import org.polytech.project.balladeapp5.Vector3;
 
 public class TheParser {
@@ -37,22 +38,29 @@ public class TheParser {
 		}
 		System.out.println(object.polygons.length);
 		for (int i = 0; i < object.polygons.length; i += 3) {
-			Vector3 v1 = new Vector3(object.vertices[object.polygons[i]],
-					object.vertices[object.polygons[i] + 1],
-					object.vertices[object.polygons[i] + 2]);
-			Vector3 v2 = new Vector3(object.vertices[object.polygons[i + 1]],
-					object.vertices[object.polygons[i + 1] + 1],
-					object.vertices[object.polygons[i + 1] + 2]);
-			Vector3 v3 = new Vector3(object.vertices[object.polygons[i + 2]],
-					object.vertices[object.polygons[i + 2] + 1],
-					object.vertices[object.polygons[i + 2] + 2]);
-			m.vertices.add(v1);
-			m.vertices.add(v2);
-			m.vertices.add(v3);
-			System.out.println("face : " + v1.toString() + ", " + v2.toString()
-					+ ", " + v3.toString());
+			float x = object.vertices[object.polygons[i]];
+			float y = object.vertices[object.polygons[i] + 1];
+			float z = object.vertices[object.polygons[i] + 2];
+			Vector3 p1 = new Vector3(x, y, z);
+			x = object.vertices[object.polygons[i + 1]];
+			y = object.vertices[object.polygons[i + 1] + 1];
+			z = object.vertices[object.polygons[i + 1] + 2];
+			Vector3 p2 = new Vector3(x, y, z);
+			x = object.vertices[object.polygons[i + 2]];
+			y = object.vertices[object.polygons[i + 2] + 1];
+			z = object.vertices[object.polygons[i + 2] + 2];
+			Vector3 p3 = new Vector3(x, y, z);
+			m.vertices.add(p1);
+			m.vertices.add(p2);
+			m.vertices.add(p3);
 		}
 
+		for (int i = 0; i < object.textureCoordinates.length; i += 2) {
+			float x = object.textureCoordinates[i];
+			float y = object.textureCoordinates[i + 1];
+			m.texCoords.add(new Vector2(x, y));
+		}
+		m.colors.add(new Vector3(200, 200, 200));
 		return m;
 	}
 }
